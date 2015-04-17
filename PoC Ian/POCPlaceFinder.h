04dfb5f08492c7c221a3bff57fb72a8c6195ae2f
Ian Carvalho <iancarv@gmail.com>
2015-04-17 10:15:05 -0300

    Adicionado classe PlaceFinder

---
+++ PoC Ian/POCPlaceFinder.h
@@ -0,0 +1,15 @@
+//
+//  POCPlaceFinder.h
+//  PoC Ian
+//
+//  Created by Ian Carvalho on 17/04/15.
+//  Copyright (c) 2015 Ian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface POCPlaceFinder : NSObject
+
+@property (strong, nonatomic) NSArray *places;
+
+@end
